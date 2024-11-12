215a612229f4aebd253eb8072267c1dadfe75dc6
dlavric84 <158210520+dlavric84@users.noreply.github.com>
2024-11-12 22:41:08 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,3 +1,4 @@
+#Задание N1
 name = "Дмитрий"
 print("Name:",name)
 age = 40
@@ -6,3 +7,23 @@ new_age = 40 + 1
 print("New Age:",new_age)
 is_student = True
 print ("Is Student:",is_student)
+
+#Задание N2 (Переменные)
+homework = 15                                         #Количество выполненных ДЗ
+print("Homework:",homework)
+numberOfHoursSpent = 5.4                              #Количество затраченных часов
+print("NumberOfHoursSpent:",numberOfHoursSpent)
+courseName = "Переменные"                             #Название курса
+print("CourseName:",courseName)
+timeForOneTask = (15/5.4)                             #Время на одно задание
+print("TimeForOneTask:",timeForOneTask)
+print("Курс:",courseName)
+print("Всего задач:",numberOfHoursSpent)
+print("Время на одно задание:",timeForOneTask)
+#Задание N3
+example = "kettler"
+print(example[1])                   #первый символ этой строки.
+print(example[-1])                  #последний символ этой строки (используя отрицательный индекс).
+print(example[4:])                  #вторую половину этой строки (С нечётным количеством символов
+print(example[::-1])                #это слово наоборот.
+print(example[1::2])                #каждый второй символ этой строки.
